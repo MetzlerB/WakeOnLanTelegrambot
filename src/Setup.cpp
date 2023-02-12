be58dc7d3f9ebe93ded69eeb701ef5a06bf2179e
MetzlerB <benedikt.metzler@gmail.com>
2023-02-12 13:42:05 +0100

    Tested LittleFS on Controller This was just a Test!

--- src/Setup.cpp
+++ src/Setup.cpp
@@ -1,5 +1,25 @@
 #include "Setup.h"
 
 bool Setup::checkSetup(){
-    return false;
+    if(!LittleFS.begin()){
+        Serial.println("An Error has occurred while mounting LittleFS");
+        return false;
+    }
+  
+  File ApConfig = LittleFS.open("/ApConfig.txt", "r");
+    if(!ApConfig){
+        Serial.println("Failed to open ApConfig for reading");
+        return false;
+    }
+  
+    Serial.println("File Content:");
+    while(ApConfig.available()){
+        Serial.println(ApConfig.readStringUntil('\n'));
+    }
+    ApConfig.close();
+    return true;
+}
+
+void Setup::configAP(){
+    
 }
