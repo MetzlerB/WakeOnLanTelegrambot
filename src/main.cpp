18927dcf7c80e38e3b212e55e67567eaffd80d15
Bene Metzler <benedikt.metzler@gmail.com>
2023-05-22 22:48:46 +0200

    not sure what was change (a long time ago ;-) )

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@ IPAddress apIP(192,168,4,1);
 
 ESP8266WebServer server(80);
 
-int debbug_level = 0;
+int debbug_level = 2;
 
 void APSetup(){
   if(debbug_level>=1){
@@ -34,6 +34,33 @@ void APSetup(){
     html += "<form action='/connect' method='post'>";
     html += "SSID: <input type='text' name='ssid'><br>";
     html += "Password: <input type='text' name='password'><br>";
+    html += "<input type='submit' value='Connect'>";
+    html += "</form></body></html>";
+    server.send(200, "text/html", html);
+  });
+
+  server.on("/connect",[](){
+    String ssid = server.arg("ssid");
+    String password = server.arg("password");
+    WiFi.begin(ssid.c_str(),password.c_str());
+    while(WiFi.status() != WL_CONNECTED){
+      delay(1000);
+      if (WiFi.status() != WL_CONNECTED){
+      APSetup();
+      break;
+      }
+    }
+    server.send(200, "text/plain","Wifi is now connected");
+    String html = "<html><body>";
+    html += "<form action='/TelegramSetup' method='post'>";
+    html += "<input type='submit' value='Connect'>";
+    html += "</form></body></html>";
+    server.send(200, "text/html", html);
+  });
+
+  server.on("/TelegramSetup", []() {
+    String html = "<html><body>";
+    html += "<h1>Telegram Setup</h1>";
     html += "BOT_TOKEN: <input type='text' name='bot_token'><br>";
     html += "ALLOWED_ID: <input type='text' name='allowed_id'><br>";
     html += "MAC_ADDRESS: <input type='text' name='mac_address'><br>";
@@ -43,21 +70,13 @@ void APSetup(){
   });
 
   server.on("/connect",[](){
-    String ssid = server.arg("ssid");
-    String password = server.arg("password");
     String BOT_TOKEN = server.arg("bot_token");
     String ALLOWED_ID = server.arg("allowed_id");
     String MAC_ADDRESS = server.arg("mac_address");
-    WiFi.begin(ssid.c_str(),password.c_str());
-    while (WiFi.status() != WL_CONNECTED){
-      delay(500);
-    }
     if(debbug_level >=1){
-      Serial.println("Connected to Wifi");
-      Serial.print("IP: ");
-      Serial.println(WiFi.localIP());
+      Serial.println("Got Telegram Info");
     }
-    server.send(200, "text/plain","Wifi connection established");
+    server.send(200, "text/plain","Telegram Info collected");
   });
   server.begin();
 
@@ -68,7 +87,9 @@ void setup() {
   Serial.begin(115200);
   delay(10);
   //Check if already connected to a Wifi
-  if(WiFi.status()==WL_CONNECTED){
+  if(WiFi.status()!=WL_CONNECTED){
+    APSetup();
+  }else{
     if(debbug_level>=1){
       Serial.println("Already connected");
       Serial.print("Connectet IP: ");
@@ -76,8 +97,6 @@ void setup() {
     }
     return;
   }
-
-  APSetup();
 
 }
 
